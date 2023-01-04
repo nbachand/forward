1e75d0894f45799e1c438ef17904954e14fa6779
Nicholas <ngbachand@gmail.com>
2023-01-04 14:03:50 -0800

    Integrating multiplexing line-by-line. Probably better to use ssh config

--- start.sh
+++ start.sh
@@ -5,6 +5,13 @@
 #               bash start.sh sherlock/singularity-jupyter /home/users/raphtown
 #               bash start.sh sherlock/singularity-jupyter /home/users/raphtown
 
+# Create ssh-mux (SSH multiplex) dir only if not exists
+[[ ! -d dir ]] || mkdir ~/.ssh/control/%C
+
+# Solution: Start SSH multiplexing session (works fine)
+# https://unix.stackexchange.com/questions/50508/reusing-ssh-session-for-repeated-rsync-commands
+ssh -nNf -o ControlMaster=yes -o ControlPath="~/.ssh/control/%C" nbachand@login.sherlock.stanford.edu
+
 if [ ! -f params.sh ]
 then
     echo "Need to configure params before first run, run setup.sh!"
@@ -33,16 +40,16 @@ SBATCH="$NAME.sbatch"
 
 # set FORWARD_SCRIPT and FOUND
 set_forward_script
-check_previous_submit
+check_previous_submit 'ControlPath="~/.ssh/control/%C"'
 
 echo
 echo "== Getting destination directory =="
-RESOURCE_HOME=`ssh ${RESOURCE} pwd`
-ssh ${RESOURCE} mkdir -p $RESOURCE_HOME/forward-util
+RESOURCE_HOME="ssh -o 'ControlPath="~/.ssh/control/%C"' ${RESOURCE} pwd"
+ssh -o 'ControlPath="~/.ssh/control/%C"' ${RESOURCE} mkdir -p $RESOURCE_HOME/forward-util
 
 echo
 echo "== Uploading sbatch script =="
-scp $FORWARD_SCRIPT ${RESOURCE}:$RESOURCE_HOME/forward-util/
+rsync -e "ssh -o 'ControlPath="~/.ssh/control/%C"'" $FORWARD_SCRIPT ${RESOURCE}:$RESOURCE_HOME/forward-util/
 
 # adjust PARTITION if necessary
 set_partition
@@ -61,13 +68,13 @@ command="sbatch
     $RESOURCE_HOME/forward-util/$SBATCH_NAME $PORT \"${@:2}\""
 
 echo ${command}
-ssh ${RESOURCE} ${command}
+ssh -o 'ControlPath="~/.ssh/control/%C"' ${RESOURCE} ${command}
 
 # Tell the user how to debug before trying
 instruction_get_logs
 
 # Wait for the node allocation, get identifier
-get_machine
+get_machine 'ControlPath="~/.ssh/control/%C"'
 echo "notebook running on $MACHINE"
 
 sleep $CONNECTION_WAIT_SECONDS
@@ -88,3 +95,5 @@ echo "1. Password, output, and error printed to this terminal? Look at logs (see
 echo "2. Browser: http://$MACHINE:$PORT/ -> http://localhost:$PORT/..."
 echo "3. To end session: bash end.sh ${NAME}"
 
+# Finish SSH multiplexing session
+ssh -O exit -o ControlPath="~/.ssh/control/%C" nbachand@login.sherlock.stanford.edu
